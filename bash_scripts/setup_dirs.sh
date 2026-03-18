f63aa956c3b9cfcf40b8251189efae58271fe456
OfirW3 <ofir.d.weit@gmail.com>
2026-03-18 14:49:13 +0200

    Updated setup directories script

--- bash_scripts/setup_dirs.sh
+++ bash_scripts/setup_dirs.sh
@@ -4,12 +4,10 @@ set -e
 # Create the data directories in the project root (one level above bash_scripts)
 BASE_DIR="../data"
 PCAP_DIR="$BASE_DIR/pcaps"
-RAW_DIR="$BASE_DIR/raw_csvs"
-CLEAN_DIR="$BASE_DIR/cleaned_csvs"
+RAW_DIR="$BASE_DIR/csvs"
 
-mkdir -p "$PCAP_DIR" "$RAW_DIR" "$CLEAN_DIR"
+mkdir -p "$PCAP_DIR" "$RAW_DIR"
 
 echo "Created (or verified) directories:"
 echo "  $PCAP_DIR"
 echo "  $RAW_DIR"
-echo "  $CLEAN_DIR"
